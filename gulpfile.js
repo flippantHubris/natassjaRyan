1f5c41758f838e9a4811fea571d06dffe80d56c4
Shane Brown <flippanthubris@gmail.com>
2016-10-31 05:04:02 -0500

    added dependency rimraf and clean function to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
+
+
 var gulp = require('gulp');
+
+
+// Port to use for the development server.
+var PORT = 8000;
+
+// File paths to various assets are defined here.
+var PATHS = {
+  sass: [
+    'src/assets/scss/*.scss',
+    'src/assets/scss/**/*.scss'
+  ],
+  javascript: [
+    'src/assets/js/**/*.js',
+    'src/assets/js/*.js'
+  ]
+
+};
+
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+
+//clean function: basically just deletes the dist dir
+gulp.task('clean', function(done) {
+  rimraf('dist', done);
 });
