f8304b9ae4754dd4fd3cc2b17bf3c966a8f9bda5
Shane Brown <flippanthubris@gmail.com>
2016-11-01 05:44:47 -0500

    added pages task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var rimraf = require('rimraf');
 
 var $ = require('gulp-load-plugins')();
 
+var panini = require('panini');
+
 
 // Port to use for the development server.
 var PORT = 8000;
@@ -50,7 +52,7 @@ gulp.task('img', function() {
     .pipe(gulp.dest('dist/img/'));
 });
 
-
+//compile and copy sass to dist
 gulp.task('styles', function() {
   return gulp.src([
     'src/styles/*.scss'
@@ -58,3 +60,24 @@ gulp.task('styles', function() {
     .pipe($.sass().on('error', $.sass.logError))
     .pipe(gulp.dest('dist/styles'));
 });
+
+
+// Copy page templates into finished HTML files
+gulp.task('pages', function() {
+  var injectFiles = gulp.src('dist/**/*.{css,js}');
+
+  var injectOptions = {
+    addRootSlash: false,
+    ignorePath: ['src', 'dist']
+  };
+
+    gulp.src('src/*.html')
+      .pipe(panini({
+        root: 'src/',
+        layouts: 'src/assets/layouts/',
+        partials: 'src/assets/partials/',
+      }))
+      .pipe($.inject(injectFiles, injectOptions))
+      .pipe(gulp.dest('dist'));
+  });
+  //end pages
